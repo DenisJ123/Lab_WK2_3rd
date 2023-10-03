cf797394858ef5c4457804479583ac5d9708041e
DenisJ <G00403126@atu.ie>
2023-10-03 16:29:06 +0100

    Step4

--- src/main/java/ie/atu/lab1/RequestController.java
+++ src/main/java/ie/atu/lab1/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,9 @@ public class RequestController {
     public String hello(){
         return"Hello,World";
     }
+    @GetMapping("/greet/{name}")
+    public String greetByName (@PathVariable String name){
+        return "User Name: " + name;
+    }
+
 }
